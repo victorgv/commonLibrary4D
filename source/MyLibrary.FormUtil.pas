f8c35774961325fb0236a98bd58941c0f09558c9
Victor Garcia Velasco <victorgv@gmail.com>
2021-05-12 19:02:30 +0200

    form utils

--- source/MyLibrary.FormUtil.pas
+++ source/MyLibrary.FormUtil.pas
@@ -2,8 +2,12 @@
 
 interface
 
-uses System.SysUtils,  System.Classes,
-     FMX.Forms, FMX.TabControl, System.UITypes;
+uses System.SysUtils,
+     System.Classes,
+     FMX.Forms,
+     FMX.TabControl,
+     System.UITypes,
+     FMX.Layouts;
 
 
 
@@ -14,21 +18,29 @@ type
     // ------
     FSuccProc: TProc<TModalResult>;
   protected
-
   public
     procedure RunFormAsModal(const ResultProc: TProc<TModalResult>);
-    //
     constructor create(aowner: TComponent); virtual;
   end;
 
 // --------------------------------------------------------------------------------------------
 
-type TMyLibrary_ClassOfForm = class of TMyLibrary_FormBase;
+type
+  TMyLibrary_FormTabbed = class(TMyLibrary_FormBase)
+  private
+  public
+    function getFormLayout: TLayout; virtual; abstract; // Descendant must implement to return main layout of form
+  end;
+
+type
+  TMyLibrary_ClassOfFormTabbed = class of TMyLibrary_FormTabbed;
+
+
 
 Type
   TMyLibraryTabsArrayRecord = array of record
     TabItem: TTabItem;
-    TabForm: TMyLibrary_FormBase;
+    TabForm: TMyLibrary_FormTabbed;
     ActiveFormTabByClass: TClass;
     IsbtnMenuVisible: Boolean;
     IsbtnBackVisible: Boolean;
@@ -40,12 +52,13 @@ type
   private
     FTabsArray: TMyLibraryTabsArrayRecord;
     //
-    function checkFormIsAlreadyOpen(p_formClass: TMyLibrary_ClassOfForm): integer;
+    function checkFormIsAlreadyOpen(p_formClass: TMyLibrary_ClassOfFormTabbed): integer;
   protected
     function getTabControl: TTabControl; virtual; abstract;
   public
     property TabsArray: TMyLibraryTabsArrayRecord read FTabsArray;
-    procedure RunFormAsTab(p_formClass: TMyLibrary_ClassOfForm; p_swiCanRepeat: boolean = FALSE);
+    procedure RunFormAsTab(p_formClass: TMyLibrary_ClassOfFormTabbed; p_swiCanRepeat: boolean = FALSE);
+    procedure CloseAllTabs;
   end;
 
 
@@ -54,10 +67,10 @@ implementation
 
 { TMyLibraryFormBase_MDI_Tabbed }
 
-uses uFmxUsers, uFmxMain;
+uses  uFmxMain;
 
 // Check if form already exist if then return the index of position on de TabsArray, if not returns -1
-function TMyLibrary_MainFormBase_Tabbed.checkFormIsAlreadyOpen(p_formClass: TMyLibrary_ClassOfForm): integer;
+function TMyLibrary_MainFormBase_Tabbed.checkFormIsAlreadyOpen(p_formClass: TMyLibrary_ClassOfFormTabbed): integer;
 begin
 
   // TO-DO
@@ -66,7 +79,21 @@ begin
 end;
 
 
-procedure TMyLibrary_MainFormBase_Tabbed.RunFormAsTab(p_formClass: TMyLibrary_ClassOfForm; p_swiCanRepeat: boolean);
+procedure TMyLibrary_MainFormBase_Tabbed.CloseAllTabs;
+var
+  i: integer;
+begin
+  for i:= 0 to Length(TabsArray) - 1 do
+  begin
+    TabsArray[i].TabForm.Close;
+    TabsArray[i].TabForm.getFormLayout.Parent := nil;
+    TabsArray[i].TabForm.Release; // Do somethings and free
+    TabsArray[i].TabForm:= nil;
+  end;
+end;
+
+
+procedure TMyLibrary_MainFormBase_Tabbed.RunFormAsTab(p_formClass: TMyLibrary_ClassOfFormTabbed; p_swiCanRepeat: boolean);
 var
   i: integer;
 begin
@@ -75,20 +102,17 @@ begin
 
   if i = -1 then
   begin
-    i := Length(FTabsArray) + 1;
-    SetLength(FTabsArray, i);
+    i := Length(FTabsArray);
+    SetLength(FTabsArray, i + 1);
     //FTabControl.Add()
     TabsArray[i].TabItem:= TTabItem.Create(getTabControl); //getTabControl.add;
     TabsArray[i].TabItem.Parent:= getTabControl;
     //TabsArray[i].TabItem.TabControl := getTabControl;
     getTabControl.ActiveTab := TabsArray[i].TabItem;
     TabsArray[i].TabItem.AutoSize:= False;
-    TabsArray[i].TabForm:= p_formClass.Create(TabsArray[i].TabItem);
-    TfmxUsers(TabsArray[i].TabForm).Layout1.Parent := TabsArray[i].TabItem;
-    //TfmxUsers(TabsArray[i].TabForm).Parent := TabsArray[i].TabItem;
-    //TabsArray[i].TabForm.Show;
+    TabsArray[i].TabForm:= p_formClass.Create(NIL);
+    TabsArray[i].TabForm.getFormLayout.Parent := TabsArray[i].TabItem;
   end;
-
 end;
 
 { TMyLibrary_FormBase }
